da7438cf582bf06b77aafa0057980c521f31bcea
laloparkour <laloparkour09@gmail.com>
2022-10-13 23:59:34 -0600

    PRUEBA

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 	session_start();
 
-	if (isset($_SESSION['usuario'])) {
+	if (isset($_SESSION['usuaria'])) {
 		header('Location: contenido.php');
 	} else {
 		header('Location: registrate.php');
