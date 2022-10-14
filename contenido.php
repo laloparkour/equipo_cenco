e13c2df7f1a3862aaee48fbb62062561714ce487
laloparkour <laloparkour09@gmail.com>
2022-10-14 00:02:55 -0600

    Hola kugue prueba

--- contenido.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-if (isset($_SESSION['usuario'])) {
-	require 'views/contenido.view.php';
-} else {
-	header('Location: login.php');
-}
-
-?>
--- login.php
+++ login.php
@@ -1,36 +1,5 @@
 <?php 
-	
-	session_start();
 
-	include('configdb.php');
-
-	if (isset($_SESSION['usuario'])) {
-		header('Location: index.php');
-	}
-
-	$errores = '';
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$usuario = filter_var(strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);
-		$password = $_POST['password'];
-		$password = hash('sha512', $password);
-
-		$sql = 'SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password';
-		$statement = $conexion->prepare($sql);
-		$statement->execute(array(
-			':usuario' => $usuario,
-			':password' => $password
-		));
-
-		$resultado = $statement->fetch();
-		if ($resultado !== false) {
-			$_SESSION['usuario'] = $usuario;
-			header('Location: index.php');
-		} else {
-			$errores .= '<li>Datos Incorrectos</li>';
-		}
-	}
-
-	require 'views/login.view.php';
-
+	echo "Hola kugue";
 ?>
+
